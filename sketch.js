2fb2a32dc12bb89a3f0f7b1af2cd8aa0ab94918f
olygamy <olygamy@gmail.com>
2016-06-25 20:17:23 +0100

    master files

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+
+function setup() {
+  createCanvas (windowWidth, windowHeight);
+
+ background(0);
+
+  for(var i = 10; i < width; i += 10) {
+    // If 'i' divides by 20 with no remainder draw the first line
+    // else draw the second line
+    if(i%20 == 0) {
+      stroke(255);
+      line(i, 80, i, height/2);
+    } else {
+      stroke(153);
+      line(i, 20, i, 180); 
+    }
+  }
+}
