ce85a06ec71d1464f1907ced1f5c66639983aa08
olygamy <olygamy@users.noreply.github.com>
2016-06-26 00:04:26 +0100

    bubble constructor

---
+++ bubble.js
@@ -0,0 +1,18 @@
+function Bubble(x, y, col) {
+  this.x = x;
+  this.y = y;
+  this.col = col;
+
+  this.display = function() {
+    noStroke();
+    fill(this.col);
+    ellipse(this.x, this.y, 10, 10);
+  }
+
+
+  this.update = function(col) {
+    fill(this.col);
+  }
+
+
+}
