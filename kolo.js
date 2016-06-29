77a9a28d6af70fa7fcf4d0f5aec96496a47905bf
olygamy <olygamy@users.noreply.github.com>
2016-06-29 14:05:16 +0100

    Update kolo.js

--- kolo.js
+++ kolo.js
@@ -1,32 +1,15 @@
-var  a=0;
-
-function Kolo (x, y, size, col, alfa) {
+function Kolo (x, y, size, col) {
   this.x = x;
   this.y = y;
   this.col=col;
   this.size=size;
 
-  this.alfa=alfa;
 
 
   this.display = function() {
     noStroke();
     
-    fill(this.col, this.alfa);
+    fill(this.col);
     ellipse(this.x, this.y, size, size);
   }
-
-
-  this.update = function(colb, a, newsize) {
-    this.alfa=a;
-    this.col=colb;
-    this.size=newsize;
-  // a=a+1;
-  //   if (a >255){
-  //     a=255;
-  //   }
-    fill(colb,a);
-  }
-
-
 }
