427c3da81151e44b89b8c16c4a1eda5663155a19
olygamy <olygamy@users.noreply.github.com>
2016-07-03 18:04:44 +0100

    update function added

--- kolo.js
+++ kolo.js
@@ -1,15 +1,29 @@
-function Kolo (x, y, size, col) {
+// var colb;
+// var sizeb;
+
+ var a = 0;
+// var millisecond = millis();
+ 
+function Kolo(x, y, size, col) {
   this.x = x;
   this.y = y;
-  this.col=col;
-  this.size=size;
+  this.col = col;
+  this.size = size;
+ this.display = function() {
 
-
-
-  this.display = function() {
-    noStroke();
-    
-    fill(this.col);
-    ellipse(this.x, this.y, size, size);
+  noStroke();
+  fill(this.col,a);
+  ellipse(this.x, this.y, this.size, this.size);
   }
+
+  this.update = function(colb, sizeb) {
+   this.colb = colb;
+    this.sizeb = sizeb;
+
+    noStroke();
+    fill(colb, a);
+     ellipse(this.x, this.y, this.sizeb, this.sizeb);
+
+  }
+
 }
